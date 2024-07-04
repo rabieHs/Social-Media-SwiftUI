4238b2f435233de69d982b2f9fd309230ad38c5b
rabieHs <rabiehoussaini@gmail.com>
2024-07-04 16:01:04 +0100

    Initial Commit

---
+++ Social-Media/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Social-Media
+//
+//  Created by rabie houssaini on 4/7/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Social-Media/Social_MediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_MediaApp.swift
+//  Social-Media
+//
+//  Created by rabie houssaini on 4/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct Social_MediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
